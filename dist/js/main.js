20f2c6c665d9c51d6cbad0b55d8c230018d4f78f
robotcigan <robotcigan@gmail.com>
2017-04-05 13:21:25 +0300

    05.04

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   // Замена картинок на background
   $('.direction').each(function() {
     var src = $(this).find('.direction__img').attr('src');
-    $(this).css('background', 'url(' + src + ') no-repeat')
+    $(this).css('background', 'url(' + src + ') no-repeat');
   });
 
   // Masonry сетка
@@ -24,6 +24,41 @@ $(document).ready(function () {
 
   $('.special-slider-nav .fa-angle-right').on('click', function() {
     $('.special-slider').slick("slickNext");
+  })
+
+  $('.regular-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+
+  $('.tabs__nav li').on('click', function() {
+    var index = $(this).index();
+    $('.tabs__nav li a').removeClass('active');
+    $(this).find('a').addClass('active');
+    $('.tabs__content').hide();
+    $('.tabs__content').eq(index).show();
+  });
+
+  $('.accordion__header .accordion__icon').on('click', function() {
+    $(this).toggleClass('accordion__icon--active');
+    $(this).closest('.accordion').find('.accordion__body-unvisible').stop().slideToggle()
+  });
+
+  // $('.datepicker').datepicker({
+  //   lang: 'zh-CN'
+  // })
+
+  $('.main-header').addClass('main-header--scroll')
+
+  var page = window.location.pathname;
+
+  if(page === '/_index.html') {
+    $('.main-header').addClass('main-header--black');
+  }
+
+  $('.menu-toggle').on('click', function() {
+    $(this).toggleClass('menu-toggle--active');
   })
 
 });
