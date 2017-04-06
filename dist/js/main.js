773ebad451332bab2b7473e75ffe7da1985ee6eb
robotcigan <robotcigan@gmail.com>
2017-04-06 10:01:16 +0300

    06.04

--- dist/js/main.js
+++ dist/js/main.js
@@ -49,6 +49,8 @@ $(document).ready(function () {
   //   lang: 'zh-CN'
   // })
 
+
+  // Header шапка
   $('.main-header').addClass('main-header--scroll')
 
   var page = window.location.pathname;
@@ -59,6 +61,81 @@ $(document).ready(function () {
 
   $('.menu-toggle').on('click', function() {
     $(this).toggleClass('menu-toggle--active');
-  })
+  });
+
+  var rangeSlider = document.getElementById('range-slider');
+
+  noUiSlider.create(rangeSlider, {
+    start: [1000, 7000],
+    connect: true,
+    tooltips: true,
+    range: {
+      'min': 0,
+      'max': 8000
+    }
+  });
+
+  var slider = document.getElementById('slider');
+
+  noUiSlider.create(slider, {
+    start: [7],
+    step: 1,
+    range: {
+      'min': 0,
+      'max': 10
+    }
+  });
+
+  var sliderValue = document.getElementById('slider-value');
+
+  slider.noUiSlider.on('update', function( values, handle ) {
+    sliderValue.innerHTML = values[handle];
+  });
+
+  $('.card').on('click', function() {
+    $(this).parent().find('.card__bottom').stop().slideToggle();
+  });
+
+  console.log('dgsd')
 
 });
+
+addEventListener('DOMContentLoaded', function () {
+
+  // var plus_2_days = new Date;
+  // plus_2_days.setDate(plus_2_days.getDate() + 2);
+  // pickmeup('.datepicker', {
+  //   flat      : true,
+  //   date      : [
+  //     new Date,
+  //     plus_2_days
+  //   ],
+  //   mode      : 'range',
+  //   calendars : 2
+  // });
+
+  pickmeup('.datepicker', {
+    position      : 'down',
+    mode      : 'range',
+    calendars : 2
+  });
+
+});
+
+$(document).ready(function() {
+
+
+  // Главная страница инпуты
+  $('.hero__input').on('click', function() {
+    $('.hero__input').removeClass('hero__input--active');
+    $(this).addClass('hero__input--active');
+  });
+
+  // Гости и комнаты селект
+  $('.guest-clicker__btn').on('click', function() {
+    var guestCount = $('.guest-clicker__number').text();
+    console.log(guestCount)
+    $('.guest-clicker__input').val(guestCount);
+  })
+
+})
